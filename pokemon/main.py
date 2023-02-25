ee75a2e605befbe8488720ab2a4dc4fe8232af13
eranbw123 <eranbw123@gmail.com>
2023-02-25 18:43:55 +0200

    feat: added server and dockerfile

--- pokemon/main.py
+++ pokemon/main.py
@@ -1,7 +1,12 @@
-from coche import Coach
-from pokemons import Pokemon, Stats
-import asyncio
+# from coche import Coach
+# from pokemons import Pokemon, Stats
+# import asyncio
 
+# from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
 
 # pokemon_names = ["bulbasaur", "pikachu", "charmander", "charmander"]
 # c = Coach(name="eran", pokemons=pokemon_names)
@@ -9,7 +14,7 @@ import asyncio
 # c.add_pokemons(["ditto"])
 # print(c)
 
-pic = Pokemon("test", 1, ["1", "2"], Stats(1, 2, 3, 4, 5, 6))
-pokemons = asyncio.run(pic.fetch_pokemons(["test"]))
 
-print(pokemons)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
